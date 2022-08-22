2671487629d8b22dec2d0365b9e9e94897dfdc3b
sienu47 <godrkdgh@naver.com>
2022-08-22 18:43:43 +0900

    Fix Cloudmap port 수정
    custom_attribute 삭제, port 추가

--- terraform/Cloudmap-namspace.tf
+++ terraform/Cloudmap-namspace.tf
@@ -1,34 +1,34 @@
-# resource "aws_vpc" "example" {
-#   cidr_block = "10.0.0.0/16"
-# }
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
 
-# resource "aws_service_discovery_private_dns_namespace" "namespace" {
-#   name        = "Private"
-#   description = "namespace-user"
-#   vpc         = aws_vpc.vpc.id
-# }
+resource "aws_service_discovery_private_dns_namespace" "namespace" {
+  name        = "Private"
+  description = "namespace-user"
+  vpc         = aws_vpc.vpc.id
+}
 
-# resource "aws_service_discovery_service" "namespace-service" {
-#   name = "Information"
+resource "aws_service_discovery_service" "namespace-service" {
+  name = "Information"
 
-#   dns_config {
-#     namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
 
-#     dns_records {
-#       ttl  = 300
-#       type = "A"
-#     }
+    dns_records {
+      ttl  = 300
+      type = "A"
+    }
 
-#     routing_policy = "WEIGHTED"
-#   }
-# }
+    routing_policy = "WEIGHTED"
+  }
+}
 
-# resource "aws_service_discovery_instance" "ns-service-instance" {
-#   instance_id = "Private-EC2"
-#   service_id  = aws_service_discovery_service.namespace-service.id
+resource "aws_service_discovery_instance" "ns-service-instance" {
+  instance_id = "Private-EC2"
+  service_id  = aws_service_discovery_service.namespace-service.id
 
-#   attributes = {
-#     AWS_INSTANCE_IPV4 = aws_instance.USEREC202.private_ip
-#     custom_attribute  = "custom"
-#   }
-# }
+  attributes = {
+    AWS_INSTANCE_IPV4 = aws_instance.USEREC202.private_ip
+    AWS_INSTANCE_PORT = 80
+  }
+}
